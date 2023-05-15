ea1c0e8c794e7c093613eb6020330b2bdeba17bf
Jakub Urbański <kuba.urban01@gmail.com>
2023-05-16 00:12:43 +0200

    Add roslyn sandbox

---
+++ Docgen.RoslynSandbox/CustomWalker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Text;
+
+namespace CoreLib.RoslynSandbox;
+
+public class CustomWalker : CSharpSyntaxWalker
+{
+    private readonly StringBuilder _sb;
+    public string Code => _sb.ToString();
+
+    public CustomWalker()
+        : base(SyntaxWalkerDepth.Token)
+    {
+        _sb = new StringBuilder();
+    }
+
+    public override void VisitToken(SyntaxToken token)
+    {
+        _sb.Append(token.ToFullString());
+
+        base.VisitToken(token);
+    }
+}
---
+++ Docgen.RoslynSandbox/Program.cs
@@ -0,0 +1,19 @@
+﻿using CoreLib.RoslynSandbox;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+
+MSBuildLocator.RegisterDefaults();
+using var workspace = MSBuildWorkspace.Create();
+
+var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName, "CoreLib.Example.csproj");
+var project = await workspace.OpenProjectAsync(path);
+
+var file = project.Documents.Single(x => x.Name == "Payment.cs");
+var tree = await file.GetSyntaxTreeAsync();
+var root = tree?.GetRoot();
+
+var walker = new CustomWalker();
+walker.Visit(root);
+
+Console.WriteLine(walker.Code);
+Console.ReadKey();
