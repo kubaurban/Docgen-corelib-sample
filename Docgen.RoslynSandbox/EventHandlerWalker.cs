8f45ff540e0650a6aedd7f24eaa2b2b7c80d8bd0
Jakub Urbański <kuba.urban01@gmail.com>
2023-06-13 21:18:43 +0200

    Add roslyn walker identifying CommandHandlers

---
+++ Docgen.RoslynSandbox/EventHandlerWalker.cs
@@ -0,0 +1,33 @@
+﻿using LeanCode.CQRS.Execution;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+namespace CoreLib.RoslynSandbox;
+
+public class EventHandlerWalker : CSharpSyntaxWalker
+{
+    private readonly StringBuilder _sb;
+    private readonly SemanticModel _model;
+
+    public string Code => _sb.ToString();
+
+    public EventHandlerWalker(SemanticModel model)
+    {
+        _model = model;
+        _sb = new StringBuilder();
+    }
+
+    public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+    {
+        var symbol = _model.GetDeclaredSymbol(node);
+        
+        if (symbol is not null && symbol.Interfaces.Any(x => x.MetadataName == typeof(ICommandHandler<,>).Name))
+        {
+            _sb.AppendLine(symbol.Name);
+        }
+
+        base.VisitClassDeclaration(node);
+    }
+}
--- Docgen.RoslynSandbox/Program.cs
+++ Docgen.RoslynSandbox/Program.cs
@@ -1,4 +1,6 @@
-﻿using CoreLib.RoslynSandbox;
+﻿using CoreLib.Example;
+using CoreLib.RoslynSandbox;
+using LeanCode.CQRS.Execution;
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis.MSBuild;
 
@@ -8,12 +10,30 @@ using var workspace = MSBuildWorkspace.Create();
 var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName, "CoreLib.Example.csproj");
 var project = await workspace.OpenProjectAsync(path);
 
-var file = project.Documents.Single(x => x.Name == "Payment.cs");
-var tree = await file.GetSyntaxTreeAsync();
-var root = tree?.GetRoot();
+// ----------- echo Payment.cs file -----------
+//var file = project.Documents.Single(x => x.Name == "Payment.cs");
+//var tree = await file.GetSyntaxTreeAsync();
+//var root = tree?.GetRoot();
 
-var walker = new CustomWalker();
-walker.Visit(root);
+//var walker = new CustomWalker();
+//walker.Visit(root);
 
-Console.WriteLine(walker.Code);
+//Console.WriteLine(walker.Code);
+
+// ----------- lists all CommandHandlers -----------
+var compilation = await project.GetCompilationAsync();
+if (compilation == null) return;
+
+foreach (var file in project.Documents)
+{
+    var tree = await file.GetSyntaxTreeAsync();
+    var root = tree?.GetRoot();
+
+    var walker = new EventHandlerWalker(compilation.GetSemanticModel(tree));
+    walker.Visit(root);
+
+    Console.Write(walker.Code);
+}
+
 Console.ReadKey();
+
